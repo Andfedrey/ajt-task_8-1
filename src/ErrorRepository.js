6a0271394e0caaabb260356dcc025db8fa2fdbb0
Andrew Fedorov <119888564+Andfedrey@users.noreply.github.com>
2023-02-21 13:53:12 +0300

    Update ErrorRepository.js

--- src/ErrorRepository.js
+++ src/ErrorRepository.js
@@ -3,10 +3,6 @@ export default class ErrorRepository {
     this.collection = new Map();
   }
 
-  generateNewError(key, value) {
-    this.collection.set(key, value)
-  }
-
   translate(errorKey) {
     return this.collection.has(errorKey) ? this.collection.get(errorKey) : 'Unknown error'
   }
